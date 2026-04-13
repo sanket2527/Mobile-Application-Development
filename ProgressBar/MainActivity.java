7d5ef3ef54bc3181aad4fe3136c916592ce20908
sanket2527 <sanketvharambale2527@gmail.com>
2026-04-13 20:47:34 +0530

    Update MainActivity.java

--- ProgressBar/MainActivity.java
+++ ProgressBar/MainActivity.java
@@ -1,18 +1,16 @@
-package com.example.yourapp;
+package com.example.progressbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     ProgressBar horizontalBar;
-    TextView txtProgress;
     Button btnNext;
 
     int progress = 0;
@@ -24,35 +22,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         horizontalBar = findViewById(R.id.horizontalBar);
-        txtProgress = findViewById(R.id.txtProgress);
         btnNext = findViewById(R.id.btnNext);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (progress < 100) {
-                    progress += 2;
-
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            horizontalBar.setProgress(progress);
-                            txtProgress.setText(progress + "%");
-                        }
-                    });
-
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+        startProgress();
 
         btnNext.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, CircularActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void startProgress() {
+        progress = 0;
+
+        new Thread(() -> {
+            while (progress <= 100) {
+                try {
+                    Thread.sleep(50);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                handler.post(() -> horizontalBar.setProgress(progress));
+                progress++;
+            }
+        }).start();
     }
 }
